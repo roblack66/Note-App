51f0b71e1840aee6febadb8aea8d5552e9ad87f9
roblack66 <romeoasante66@gmail.com>
2025-08-08 20:43:18 +0000

    update

--- Routes/user.js
+++ Routes/user.js
@@ -1,9 +1,10 @@
 import { Router } from "express";
-import { registerUser } from "../Controllers/user.js";
+import { loginUser, registerUser } from "../Controllers/user.js";
 
 
 const userRouter = Router()
 
 userRouter.post('/users/register', registerUser)
+userRouter.post('/users/login', loginUser)
 
 export default userRouter
